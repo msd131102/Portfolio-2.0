8141c66ae282dc5daab93fa24e826f529f3333a6
Shiva <96329889+msd131102@users.noreply.github.com>
2025-04-06 21:54:24 +0530

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    devIndicators: false,  // Disable DevTools Indicator completely
+  }
+  
+  module.exports = nextConfig
+  
